727724504fcddb17189c628736091a3192d9d718
Johnathan Martin <john.martin@stanford.edu>
2019-08-05 16:20:33 -0700

    improved help text and logging

--- src/cli/exportAndSave.js
+++ src/cli/exportAndSave.js
@@ -5,27 +5,35 @@ import { downloadAllRdfForGroup } from '../getGroupRDF'
 
 
 const helpText = `
-> ./bin/export 'ucdavis'
+Invoke the bin/export script with the name of the group to be exported
+as a parameter.  E.g. to export RDF for the ucdavis group:
 
-and you should end up with something like:
+$ ./bin/export 'ucdavis'
 
-$ tree sinopia_export/
-sinopia_export/
-└── ucdavis_2019-07-26T01:41:00.002Z
+...which should leave you with output like:
+
+$ tree exported_rdf/
+exported_rdf/
+└── ucdavis_2019-08-05T23:11:54.601Z
     ├── 0c9895ff-9470-4c70-bee4-b791ee179b34
     └── complete.log
 
-each RDF resource will have its own file.  complete.log will be written at the end of a successful run.
+* The output will land in the configured exportBasePath (defaults to
+  './exported_rdf'), in a subdirectory named for the exported group and
+  the time at which the export was run.
+* Each RDF resource will have its own file (named for the resource).
+* complete.log will be written at the end of a successful run.
 `
 
 
-console.debug(`arguments: ${process.argv}`)
 console.info(`sinopia_exporter v${sinopiaExporter.version}`)
-
 
 const groupName = process.argv[2]
 if(groupName) {
+  console.info(`beginning export of RDF from group: ${groupName}`)
   downloadAllRdfForGroup(groupName)
+  console.info(`finished export of RDF from group: ${groupName}`)
 } else {
   console.error(helpText)
+  console.error('\nIt appears that no group name was specified.')
 }
