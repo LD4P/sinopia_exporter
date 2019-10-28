2b3b103f95188acd2553c6cd03d7cdeeb99f1be1
Justin Littman <justinlittman@stanford.edu>
2019-10-28 12:53:02 -0400

    Adds upload to S3.
    refs #22

--- README.md
+++ README.md
@@ -5,8 +5,18 @@ Docker export performs an export of all groups, zips up the results, and exports
 
 To build and run the Docker container:
 ```
-docker build . -t "ld4p/export"
-docker run --rm "ld4p/export"
+docker build . -t "ld4p/sinopia_exporter"
+docker run --rm "ld4p/sinopia_exporter"
+```
+
+To push:
+```
+docker push ld4p/sinopia_exporter
 ```
 
 For local testing purposes, you may want to link in a local directory (`-v`) to view the results.
+
+### Configuration
+The export container uses the following environment variables:
+* `S3_BUCKET`: For example, sinopia-exports-development
+* `TRELLIS_BASEPATH`: For example, https://trellis.development.sinopia.io
--- bin/docker_export.sh
+++ bin/docker_export.sh
@@ -4,21 +4,32 @@ set -e
 rm -fr exported_rdf/*
 
 sh bin/export '_ALL_'
+echo "Done exporting"
+
+if [ -n "$S3_BUCKET" ]; then
+  echo "Emptying bucket"
+  aws s3 rm s3://$S3_BUCKET --recursive
+fi
 
 cd exported_rdf
 ALLFILES=sinopia_export_all*
 for allfile in $ALLFILES
 do
-  echo $allfile
-  zip -r $allfile.zip $allfile/
+  zip -q -r $allfile.zip $allfile/
+  if [ -n "$S3_BUCKET" ]; then
+    echo "Sending to S3"
+    aws s3 cp $allfile.zip s3://$S3_BUCKET --acl public-read
+  fi
+
   cd $allfile
   GROUPFILES=*_*
   for groupfile in $GROUPFILES
   do
-    echo $groupfile
-    zip -r $groupfile.zip $groupfile/
+    zip -q -r $groupfile.zip $groupfile/
+    if [ -n "$S3_BUCKET" ]; then
+      echo "Sending to S3"
+      aws s3 cp $groupfile.zip s3://$S3_BUCKET --acl public-read
+    fi
   done
-  cd ..
-  mv $allfile/*.zip .
 done
 echo "Done"
